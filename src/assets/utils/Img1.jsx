7dc38df1d04c45180c0757d3cbb3a12906273446
Fede <ferrelli.mazza@gmail.com>
2022-08-16 20:08:18 -0300

    Add Images Carrousel

--- src/assets/utils/Img1.jsx
+++ src/assets/utils/Img1.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import Imagen from '../images/cafe_black.jpg'
+import React, {useState, useEffect} from 'react';
+import {Fade} from 'react-awesome-reveal'
+import CafeBlack from '../images/cafe_black.jpg';
+import CupCake from '../images/cup_cake.jpg';
+import Cheers from '../images/cheers.jpg';
+import Milk from '../images/milk.jpg';
+import Pouring from '../images/pouring_coffee.jpg';
+import Brunch from '../images/brunch.jpg'
 
 function Img1() {
+    const imagenes = [CafeBlack, CupCake, Cheers, Milk, Pouring, Brunch]
+    const [Imagen, setImagen] = useState(0)
+    
+useEffect(() => {
+
+    const max = imagenes.length-1;
+    let counter=0; 
+
+    const changeImage = () =>{
+
+        if (Imagen == max){
+            counter = 0;
+            setImagen(0)
+        } else {
+            counter = counter+1;
+        setImagen(Imagen+1)}
+
+    };
+
+    setTimeout(changeImage, 3000);
+
+    console.log(Imagen)
+    
+}, [Imagen])
+
+
+
+
     return (
-        <div>
-            <img src={Imagen} />
+        <div className="w-full h-80 overflow-hidden py-4">
+       
+            <img src={imagenes[Imagen]} />
+            
         </div>
     )
 }
