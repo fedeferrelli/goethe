178138d0f18b1498e2592e52c98b252b2d61e05a
Fede <ferrelli.mazza@gmail.com>
2022-08-20 11:10:39 -0300

    Resttyle product carrousel

---
+++ src/assets/utils/productsToShow.jsx
@@ -0,0 +1,30 @@
+ import Image from '../images/carrousel.jpg'
+
+
+
+const productsToShow = [
+
+    {name: 'Cafe negro ', 
+    image: Image, 
+    descripcion : 'Café expreso y fina capa de leche texturizada texturizada texturizada texturizada texturizada texturizada',
+     precio : 80},
+
+     {name: 'Cafe negro Power', 
+     image: Image, 
+     descripcion : 'Café expreso intenso',
+      precio : 480},
+
+      {name: 'Cafe en Taza amarilla', 
+    image:Image, 
+    descripcion : 'Café servido en lña mejor taza del condado',
+     precio : 480},
+
+     {name: 'Cafecito', 
+    image:Image, 
+    descripcion : 'Café para compratir en cualquier momento del día',
+     precio : 480.5},
+
+
+];
+
+export default productsToShow;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from 'react';
+
+import CafeBlack from '../assets/images/cafe_black.jpg';
+import CupCake from '../assets/images/cup_cake.jpg';
+import Cheers from '../assets/images/cheers.jpg';
+import Milk from '../assets/images/milk.jpg';
+import Pouring from '../assets/images/pouring_coffee.jpg';
+import Brunch from '../assets/images/brunch.jpg';
+
+import productsToShow from '../assets/utils/productsToShow';
 
 function Products() {
 
-    const productos = [1, 2, 3, 4]
+    const productos = [CafeBlack, CupCake, Cheers, Milk]
     return (
-        <div className="h-48  p-3 overflow-auto whitespace-nowrap">
+        <div className="h-72  p-3 overflow-auto whitespace-nowrap">
 
-{productos.map(e=>(
+{productsToShow.map(e=>(
 
-<div className="w-1/2 m-auto h-full bg-gray-200 inline-block rounded-lg border-[0.5px] border-gray-400 mr-3 shadow-md">{e}</div>
+
+<div className="w-1/2 m-auto h-full inline-block rounded-lg border-[0.5px] border-gray-400 mr-3 shadow-md overflow-hidden text-gray-800">
+    
+    <div className="h-28 overflow-hidden">
+    <img className="w-full " src={e.image}/>
+    </div>
+
+    <section className="h-36 flex flex-col justify-between p-4 whitespace-normal">
+
+        <div className="flex flex-col">
+            <h1 className="font font-semibold tracking-wide">{e.name.slice(0,28)}</h1>
+            <p className="mt-1 text-xs  font-light inline-block">{e.descripcion.slice(0,70)}</p>
+        </div>
+
+        <div className="font font-semibold">${e.precio}</div>
+
+    </section>
+    
+    
+    </div>
 
 ))}
 
